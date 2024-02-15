abd9ffca10ab3789e748c2c42b67c5a70a077234
artemiy <art.khromts@gmail.com>
2024-02-15 16:05:20 +0300

    Initial Commit

---
+++ Everyweather/EveryweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryweatherApp.swift
+//  Everyweather
+//
+//  Created by Артемий Хромцов on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct EveryweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
