af0e119108405bb6d622fe9f2b48a03fa5425776
artemiy <art.khromts@gmail.com>
2024-02-15 16:46:48 +0300

    init commit

--- Everyweather/EveryweatherApp.swift
+++ Everyweather/EveryweatherApp.swift
@@ -1,17 +1,10 @@
-//
-//  EveryweatherApp.swift
-//  Everyweather
-//
-//  Created by Артемий Хромцов on 15.02.2024.
-//
-
 import SwiftUI
 
 @main
 struct EveryweatherApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WeatherMainScreen()
         }
     }
 }
---
+++ Everyweather/Util/Config.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Config {
+    static let weatherApiKey = ""
+    static let baseUrl = ""
+    static let geoApiKey = ""
+    static let googleMapsKey = ""
+
+
+}
